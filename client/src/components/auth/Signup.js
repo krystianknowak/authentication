72f2d53279d8e262d259b19e46ff951406ab0c7b
Krystian Nowak <krystian.k.nowak@gmail.com>
2020-07-22 16:32:48 +0200

    signun functionality

---
+++ client/src/components/auth/Signup.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { reduxForm, Field } from "redux-form";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import * as actions from "../../actions";
+
+const Signup = (props) => {
+    const { handleSubmit } = props;
+    const onSubmit = (formProps) => {
+        props.signup(formProps, () => {
+            props.history.push("/feature");
+        })
+    };
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <fieldset>
+                <label>Email</label>
+                <Field
+                    name="email"
+                    type="text"
+                    component="input"
+                    autoComplete="none"
+                />
+            </fieldset>
+            <fieldset>
+                <label>Password</label>
+                <Field
+                    name="password"
+                    type="password"
+                    component="input"
+                    autoComplete="none"
+                />
+            </fieldset>
+            <div>
+                {props.errorMessage}
+            </div>
+            <button>Sign Up!</button>
+        </form>
+    )
+}
+
+function mapStateToProps(state) {
+    return { errorMessage: state.auth.errorMessage }
+}
+
+export default compose(
+    connect(mapStateToProps, actions),
+    reduxForm({ form: "signup" })
+)(Signup);
