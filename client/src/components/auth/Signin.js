d3bf2e8ae90310436faad91cd6e8b347fa44dc88
Krystian Nowak <krystian.k.nowak@gmail.com>
2020-07-22 16:32:37 +0200

    signin functionality

---
+++ client/src/components/auth/Signin.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { reduxForm, Field } from "redux-form";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import * as actions from "../../actions";
+
+const Signin = (props) => {
+    const { handleSubmit } = props;
+    const onSubmit = (formProps) => {
+        props.signin(formProps, () => {
+            props.history.push("/feature");
+        })
+    };
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <fieldset>
+                <label>Email</label>
+                <Field
+                    name="email"
+                    type="text"
+                    component="input"
+                    autoComplete="none"
+                />
+            </fieldset>
+            <fieldset>
+                <label>Password</label>
+                <Field
+                    name="password"
+                    type="password"
+                    component="input"
+                    autoComplete="none"
+                />
+            </fieldset>
+            <div>
+                {props.errorMessage}
+            </div>
+            <button>Sign in!</button>
+        </form>
+    )
+}
+
+function mapStateToProps(state) {
+    return { errorMessage: state.auth.errorMessage }
+}
+
+export default compose(
+    connect(mapStateToProps, actions),
+    reduxForm({ form: "signin" })
+)(Signin);
